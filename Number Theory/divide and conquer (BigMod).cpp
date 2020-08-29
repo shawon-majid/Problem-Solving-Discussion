915376c06061d3f8c0973d8a10cb519f52c3d9ea
Shawon Majid <shawon.majid@gmail.com>
2020-08-29 15:22:40 +0600

    Update divide and conquer (BigMod).cpp

--- Number Theory/divide and conquer (BigMod).cpp
+++ Number Theory/divide and conquer (BigMod).cpp
@@ -16,6 +16,19 @@ typedef vector< int > vi;
 
 /*
                 Problem from mahbubul hasan contest book page 63
+          In this problem, you have to find 1+a+a^2+a^3+a^4+.....+a^n-1 Mod m
+          You're given a, n and m.
+          
+          Here, we will use Divide and Conquer Method for finding the answer.
+          For this question you need to know how to implement bigMod function.
+          If you don't know about it visit https://golammostaeen.wordpress.com/2012/10/20/big-mod-algorithm/
+          
+          Now we will divide the problem into 2 parts:
+             1 + a + a^2 + a^3 + a^4 + a^5 = (1+a+a^2) + a^3(1+a+a^2)
+             if n was odd,
+             1 + a + a^2 + a^3 + a^4 + a^5 + a^6 = (1+a+a^2) + a^3(1+a+a^2) + a^6
+          Now we will implement this idea. Let's roll!
+          
 */
 int bigMod(int a, int b, int M){
     if(b == 0) return 1 % M;
