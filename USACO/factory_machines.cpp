9592cfdce9db30677c63429af2a5e6f342598ee2
Shawon Majid <shawon.majid@gmail.com>
2021-04-17 19:19:25 +0600

    Add files via upload
    problem: https://cses.fi/problemset/result/2000067/

---
+++ USACO/factory_machines.cpp
@@ -0,0 +1,45 @@
+//Bismillahir Rahman-ir Rahim
+#include <bits/stdc++.h>
+using namespace std;
+#define debug(x) cout << '>' << #x << " : " << x << endl;
+#define all(c) c.begin(), c.end()
+#define F first
+#define S second
+typedef unsigned long long ull;
+typedef long long ll;
+
+
+int main() {
+    
+    ll n, t;
+    cin >> n >> t;
+
+    vector<ll> ara(n);
+    for(int i = 0; i < n; i++){
+        cin >> ara[i];
+    }
+
+    ll minel = *min_element(all(ara));
+
+
+
+    ll ans = LLONG_MAX;
+    ll high = t*minel, low  = 0;
+    while(low <= high) {
+        ll mid = low + (high - low)/2;
+        ll products = 0;
+        for(int i = 0; i < n; i++){
+            products += (mid/ara[i]);
+        }
+        if(products >= t){
+            ans = min(ans, mid);
+            high = mid - 1;
+        }
+        else{
+            low = mid+1;
+        }
+    }
+    cout << ans << endl;
+    
+    return 0;
+}
