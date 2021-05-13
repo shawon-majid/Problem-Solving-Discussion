683f9330ef830806abf3f8bc788643c25ed607b0
Shawon Majid <shawon.majid@gmail.com>
2021-05-13 22:43:20 +0600

    Update edu_binary_search_step_2C.cpp
    problem: https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/C

--- Codeforces/edu_binary_search_step_2C.cpp
+++ Codeforces/edu_binary_search_step_2C.cpp
@@ -3,31 +3,47 @@
 using namespace std;
 #define debug(x) cout << '>' << #x << " : " << x << endl;
 #define all(c) c.begin(), c.end()
+#define F first
+#define S second
 typedef unsigned long long ull;
 typedef long long ll;
 
-struct custom_compare final
+bool ok(ll m, ll mn, ll mx, ll n)
 {
-    bool operator() (const pair<int,int>& left, const pair<int, int>& right) const
-    {
-        return left.second < right.second;
-    }
-};
+    if(m < mn) return 0;
+    m -= mn;
+    return (m/mn) + (m/mx) + 1 >= n;
+}
 
 int main() {
 
+#ifndef ONLINE_JUDGE
     freopen("input.txt", "r" , stdin);
     freopen("output.txt", "w" , stdout);
+#endif
 
-    int n, k;
-    cin >> n >> k;
-    multiset <pair <int, int>, custom_compare > ms;
-    for(int i = 0; i < n; i++){
-        pair <int, int> x;
-        cin >> x.first >> x.second;
-        ms.insert(x);
-    }
+    ll n, x, y;
+    cin >> n >> x >> y;
+
+    ll mn = min(x, y), mx = max(x, y);
+
+    
+    ll l, r, m;
+    l = 0; // not possible 
+    r = mn*n; // possible
     
 
+    while(l+1 < r){
+        ll m = (l+r)/2;
+        if(ok(m, mn, mx, n)){
+            r = m;
+        }
+        else{
+            l = m;
+        }
+    }
+    
+    cout << r << endl;
+    
     return 0;
 }
