fff77c6e5750ab0b60cf976e0bde58f7e13706b4
Shawon Majid <shawon.majid@gmail.com>
2021-09-27 16:45:47 +0600

    Update Codeforces 1398C.cpp

--- Codeforces/Codeforces 1398C.cpp
+++ Codeforces/Codeforces 1398C.cpp
@@ -8,36 +8,37 @@ using namespace std;
 typedef unsigned long long ull;
 typedef long long ll;
 
-
-int main() {
-
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r" , stdin);
-    freopen("output.txt", "w" , stdout);
-#endif
+int main(){
     int T;
     cin >> T;
-    while(T--) {
+    while(T--){
         ll n;
         cin >> n;
         string str;
         cin >> str;
-        str  = "0"+str;
+        vector <ll> ara(n+1);
+        for(ll i = 0; i < str.size(); i++){
+            ara[i+1] = str[i] - '0';
+        }
         vector <ll> pre(n+1);
         pre[0] = 0;
-        for(ll i = 1; i <= n; i++){
-            pre[i] = pre[i-1] + (str[i] - '0');
-        }
-        ll sum = 0, ans = 0;
         map <ll, ll> m;
+        m[0]++;
+        ll ans = 0;
+        // condition is r - (l - 1) = pre[r] - pre[l - 1];
+        // so, pre[r] - r = pre[l-1] - (l - 1)
         for(ll i = 1; i <= n; i++){
-            m[pre[i-1] - i + 1]++; // portion that i need to substract
-            if(m.find(pre[i] - i) != m.end()){ // portion that i am currently adding
-                ans+= m[pre[i]- i];            // if addition == substraction than answer will be increased
+            pre[i] = pre[i-1] + ara[i];
+            ll need = pre[i] - i; 
+            if(m.find(need) != m.end()){
+                ans += m[need];
             }
+            m[need]++;
+
         }
         cout << ans << endl;
+
     }
-    
+
     return 0;
 }
