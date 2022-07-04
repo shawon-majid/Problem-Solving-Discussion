4f850d8426f31de95cea9e364487d2aa6156c053
Shawon Majid <shawon.majid@gmail.com>
2022-07-04 19:11:09 +0600

    Update disjoint set union.cpp

--- Graph & Tree/disjoint set union.cpp
+++ Graph & Tree/disjoint set union.cpp
@@ -5,40 +5,53 @@ using namespace std;
 #define all(c) c.begin(), c.end()
 #define F first
 #define S second
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 typedef unsigned long long ull;
 typedef long long ll;
 
-int captain[100000];
+const int mx = 2e5+10;
 
-void delegateCaptain(int v){
-    captain[v] = v;
-}
+vector < int > parent(mx);
+vector < int > sz(mx);
+
+int numberOfComponents;
 
 void init(int n){
     for(int i = 1; i <= n; i++){
-        delegateCaptain(i);
-    }
-}
-
-int findRepresentative(int u){
-    if(captain[u] == u){
-        return u;
-    }
-    captain[u] = findRepresentative(captain[u]);
-    return captain[u];
-}
-
-void makeFriends(int u, int v){
-    u = findRepresentative(u);
-    v = findRepresentative(v);
-    if(u != v){
-        captain[u] = v;
+        parent[i] = i;
+        sz[i] = 1;
     }
 }
 
 
-int main() {
-    
+int findSet(int a){
+    if(parent[a] == a){
+        return a;
+    }
+    parent[a] = findSet(parent[a]);
+    return parent[a];
+}
+
+void unionSet(int a, int b){
+    a = findSet(a);
+    b = findSet(b);
+
+    if(a != b){
+        if(sz[a] >= sz[b]){
+            parent[b] = a;
+            sz[a] += sz[b];
+        }
+        else{
+            parent[a] = b;
+            sz[b] += sz[a];
+        }
+        numberOfComponents--;
+    }
+}
+
+int main(){
+
+
     int n, m, q;
     cin >> n >> m >> q;
 
@@ -47,13 +60,13 @@ int main() {
     for(int i = 0; i < m; i++){
         int u, v;
         cin >> u >> v;
-        makeFriends(u, v);
+        unionSet(u, v);
     }
     while(q--){
         int u, v;
         cin >> u >> v;
 
-        if(findRepresentative(u) == findRepresentative(v)){
+        if(findSet(u) == findSet(v)){
             cout << "Friends" << endl;
         }
         else{
@@ -61,6 +74,6 @@ int main() {
         }
     }
 
-    
+
     return 0;
 }
